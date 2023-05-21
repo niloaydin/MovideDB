de99bc90d109bfcfe994498b8eed8bd693823f97
niloaydin <nilay.aydn97@gmail.com>
2023-05-22 00:53:07 +0300

    1. Create folder structure 2. Write pseudo code for login

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ const express = require("express");
 require("dotenv").config();
 const cors = require("cors");
 const path = require("path");
+const authRoutes = require("./routes/auth");
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -10,6 +11,8 @@ app.use(cors());
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
+app.use("/api/auth", authRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`);
