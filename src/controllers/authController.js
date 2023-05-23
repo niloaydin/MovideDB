25f31e0d4eb6c6e7a5d6098bbb1bef753ce7f25d
Cankut ER <cnkt.er@gmail.com>
2023-05-23 22:57:04 +0300

    promised

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,9 +1,9 @@
 const connection = require("../db");
 
 const login = async (req, res) => {
-  let result = await connection
-    .promise()
-    .query("SELECT pswrd FROM Database_Managers WHERE username='manager1'");
+  let result = await connection.query(
+    "SELECT pswrd FROM Database_Managers WHERE username='manager1'"
+  );
   console.log(result[0]);
   res.send(result[0]);
 };
--- src/db.js
+++ src/db.js
@@ -1,14 +1,11 @@
 const mysql = require("mysql2");
-const connection = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "Bp88mrzrd3ryy",
-  database: "movieDb",
-});
-
-connection.connect(function (err) {
-  if (err) throw err;
-  console.log("Connected!");
-});
+const connection = mysql
+  .createConnection({
+    host: "localhost",
+    user: "root",
+    password: "Bp88mrzrd3ryy",
+    database: "movieDb",
+  })
+  .promise();
 
 module.exports = connection;
