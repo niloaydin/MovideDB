869c048c3ba0686eb18a472d7c857220e4cba90c
Cankut ER <cnkt.er@gmail.com>
2023-05-25 19:50:51 +0300

    login logic done

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,11 +1,32 @@
 const connection = require("../db");
 
+/** @type {import("express").RequestHandler} */
 const login = async (req, res) => {
-  let result = await connection.query(
-    "SELECT pswrd FROM Database_Managers WHERE username='manager1'"
-  );
-  console.log(result[0]);
-  res.send(result[0]);
+  try {
+    const role = req.body.role;
+    let queryString;
+    const username = req.body.username;
+    switch (role) {
+      case "admin":
+        queryString = "SELECT pswrd FROM Database_Managers WHERE username=?";
+        break;
+      case "director":
+        queryString = "SELECT pswrd FROM Directors WHERE username=?";
+        break;
+      case "audience":
+        queryString = "SELECT pswrd FROM Audience WHERE username=?";
+        break;
+    }
+    let [rows, fields] = await connection.query(queryString, [username]);
+    const password = rows[0].pswrd;
+    if (password === req.body.password) {
+      res.send("Success");
+    } else {
+      throw new Error("Failed Login");
+    }
+  } catch (err) {
+    res.status(500).send(err.message);
+  }
 };
 
 module.exports = {
